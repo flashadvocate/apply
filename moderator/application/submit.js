e5b49694c28b3d27064a62ffb0a16cb096c2740c
flashadvocate <flashadvocate@gmail.com>
2014-11-22 13:02:14 -0500

    more...

--- moderator/application/submit.js
+++ moderator/application/submit.js
@@ -10,12 +10,12 @@ $(document).ready(function() {
 
             function(data) {
                 if (data['success'] === true) {
-                    $('.message').html('<div class="alert alert-success" role="alert">Your application was successfully submitted.</div>');
+                    $('.message').html('<div class="alert alert-success" role="alert"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span><span class="sr-only"> Success! </span><strong>Success!</strong> Your application was successfully submitted.</div>');
                     $('.alert').slideDown();
-                    $('#mod-app').fadeOut();
+                    $('#sub-form').fadeOut();
 
                 } else if (data['success'] === false) {
-                    $('.message').html("<div class=\"alert alert-danger\" role=\"alert\">" + data['message'] + "</div>");
+                    $('.message').html("<div class=\"alert alert-danger\" role=\"alert\"><span class=\"glyphicon glyphicon-exclamation-sign\" aria-hidden=\"true\"></span><span class=\"sr-only\"> Error:</span><strong>Error!</strong> " + data['message'] + "</div>");
                     $('.email').addClass('has-error');
                     $('.submit').addClass('disabled');
                     $('input[type="submit"]').attr('disabled', 'disabled');
