75f2d92bb3146a38edb74dd3f348a3d9aec3b8c7
flashadvocate <flashadvocate@gmail.com>
2014-11-24 10:59:36 -0500

    add theme, adjust a few things

--- public/scripts/submit.js
+++ public/scripts/submit.js
@@ -11,12 +11,12 @@ $(document).ready(function() {
             function(data) {
                 if (data['success'] === true) {
                     $('.message').html('<div class="alert alert-success" role="alert"><span class="glyphicon glyphicon-ok" aria-hidden="true"></span><span class="sr-only"> Success! </span><strong>Success!</strong> We have received your application!</div>');
-                    $('.intro').html('We appreciate your interest in becoming a member of the ASMDSS staff. Please allow 1-2 weeks for us to respond to you about your application. Even if your application is turned down, we will be in contact with you.<br /><br />If you have any additional questions, please send them to our <a href="mailto:devteam@asmdss.com">development team</a>.');
+                    $('.intro').html('We appreciate your interest in becoming a member of the ASMDSS staff. Please allow 1-2 weeks for us to respond to you about your application. Even if your application is turned down, we will be in contact with you.<br /><br />If you have any additional questions, please send them to our <a href="mailto:webteam@asmdss.com">development team</a>.');
                     $('.alert').slideDown();
                     $('#sub-form, .submit').fadeOut();
 
                 } else if (data['success'] === false) {
-                    $('.message').html("<div class=\"alert alert-danger\" role=\"alert\"><span class=\"glyphicon glyphicon-exclamation-sign\" aria-hidden=\"true\"></span><span class=\"sr-only\"> Error:</span><strong>Error!</strong> " + data['message'] + "</div>");
+                    $('.message').html("<div class=\"alert alert-danger\" role=\"alert\"><span class=\"sr-only\"> Error:</span><strong>Oops!</strong> " + data['message'] + "</div>");
                     $('.email').addClass('has-error');
                     $('.submit').addClass('disabled');
                     $('input[type="submit"]').attr('disabled', 'disabled');
--- public/templates/header.php
+++ public/templates/header.php
@@ -11,14 +11,15 @@ if (isset($_GET['parse_apps_and_email'])) {
 
 ?>
 
-
 <!DOCTYPE html>
 <html>
 <head>
 	<meta content="text/html; charset=utf-8" http-equiv="Content-Type">
 	<meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css">
-	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css">
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap.min.css"> -->
+	<link rel="stylesheet" href="public/css/bootstrap.css">
+	<link rel="stylesheet" href="public/css/shadows.css">
+	<!-- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/css/bootstrap-theme.min.css"> -->
 	<link rel="icon" type="image/png" href="public/images/favicon.ico">
 	<script src="//code.jquery.com/jquery-2.1.1.min.js"></script>
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.1/js/bootstrap.min.js"></script>
